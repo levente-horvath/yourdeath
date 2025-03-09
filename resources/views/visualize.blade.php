7fe1e5ab20179e4ae221faef3c0a659fcc609f96
Lev Horvath <levente-horvath@outlook.com>
2025-03-09 15:13:45 +0100

    started to modernize it

--- resources/views/visualize.blade.php
+++ resources/views/visualize.blade.php
@@ -2,25 +2,23 @@
 
 
 <div class="container mx-auto">
-    <h1 class="p-2 sm:p-10 text-xl font-bold text-center text-black">Statistics</h1>
+    <h1 class="p-2 sm:p-10 text-2xl font-bold text-center text-white italic"> “Death smiles at us all; all we can do is smile back.” - <span class="not-italic">Marcus Aurelius</span></h1>
     <div class="mt-4 mb-12 px-4 pt-4 pb-12 justify-between bg-white dark:bg-gray-600 shadow-sm rounded-lg">
         <div class="justify-center">
-            <div class="text-base text-xl text-center clear-both">Life expectancy in {{{session('nationality')}}}:   {{ session('data')}} years</div>
+            <div class="text-2xl text-center clear-both">Life expectancy in {{{session('nationality')}}}:   {{ session('data')}} years</div>
 
             <br>
             <br>
 
-
-            <div class="text-base text-xl text-center clear-both">You have {{session('data') - (session('currentYear') - session('birthyear')) }} years left to live</adiv>
+            <div class="text-base text-xl text-center clear-both">You have {{session('data') - (session('currentYear') - session('birthyear')) }} years left to live</div>
 
             <br>
             <br>
-
-            <div>
-                <div class="float-left bg-red-900 pl-4" style="width: {{ session('percentage') }}%;">
+            <div class="flex items-center justify-center mt-4">
+                <div class="w-1/5 bg-red-900 text-white font-bold text-center py-2 rounded-l">
                     {{ session('percentage') }}%
                 </div>
-                <div class="float-left w-1/5 bg-blue-100 pl-4" style="width: {{ 100 - session('percentage') }}%;">
+                <div class="w-4/5 bg-blue-100 text-center py-2 rounded-r" style="width: {{ 100 - session('percentage') }}%;">
                     {{ 100 - session('percentage') }}%
                 </div>
             </div>
@@ -32,7 +30,7 @@
             </x-rectangle-table>
         -->
 
-            <x-rectangle-table :boxNumber="session('age')*52+session('birthmonth')*4+session('birthday')" deadBoxColor="#B01818" basicBoxColor="#CCFFFF" :tableHeight="session('data')" :tableWidth="52" width="5px" height="5px">
+            <x-rectangle-table :boxNumber="session('age')*52+session('birthmonth')*4+session('birthday')" deadBoxColor="#B01818" basicBoxColor="#CCFFFF" :tableHeight="session('data')" :tableWidth="56" width="17px" height="17px">
             </x-rectangle-table>
 
 
